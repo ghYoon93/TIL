7405d7a0dc48d085376c44bb3a68ce3b42010882
ghYoon93 <55379220+ghYoon93@users.noreply.github.com>
2019-10-04 18:12:57 +0900

    Create WeLoveKriii.java

---
+++ algorithm/acmicpc/stage01/WeLoveKriii.java
@@ -0,0 +1,8 @@
+public class WeLoveKriii{
+    public static void main(String[] args){
+        String answer = "강한친구 대한육군";
+        for(int i = 0; i < 2; i++){
+            System.out.println(answer);
+        }
+    }
+}
