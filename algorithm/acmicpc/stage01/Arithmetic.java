b4f0c5339102623183c255bfb27f78da15cf3ae3
ghYoon93 <55379220+ghYoon93@users.noreply.github.com>
2019-10-04 18:53:58 +0900

    no10869

---
+++ algorithm/acmicpc/stage01/Arithmetic.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args){    	
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int sum = a + b;
+        int subtraction = a - b;
+        int multiplcation = a * b;
+        int rest = a % b;
+        int division = a / b;
+        System.out.println(sum);
+        System.out.println(subtraction);
+        System.out.println(multiplcation);
+        System.out.println(division);
+        System.out.println(rest);
+    }
+}
