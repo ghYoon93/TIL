de35a68d9b2af906970bfa6bef3cbb02af0b13c6
ghYoon93 <55379220+ghYoon93@users.noreply.github.com>
2019-10-04 18:49:55 +0900

    no1008

---
+++ algorithm/acmicpc/stage01/Division.java
@@ -0,0 +1,10 @@
+public class Division {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double answer = ((double)a/(double)b);
+        System.out.println(answer);
+	}
+
+}
