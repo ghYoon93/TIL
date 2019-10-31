649af254f4e6ad38a394256fb0647630fb3472b1
ghYoon93 <gh.yoon93@gmail.com>
2019-10-31 18:08:51 +0900

    Updated on 19/10/31 18:08

--- algorithm/dynamicProgramming/No10844_BottomUp.java
+++ algorithm/dynamicProgramming/No10844_BottomUp.java
@@ -3,17 +3,35 @@ package dynamicProgramming;
 import java.util.*;
 import java.math.*;
 public class No10844_BottomUp {
-   public static void main(String args[]) {
-       Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       long[] d = new long[n+1];
-       d[1] = 1;
-       if (n >= 2) {
-           d[2] = 1;
-       }
-       for (int i=3; i<=n; i++) {
-           d[i] = d[i-1] + d[i-2];
-       }
-       System.out.println(d[n]);
-   }
+	static final long mod = 1000000000L;
+	static final int MAX = 100;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[][] d = new long[MAX+1][10];
+        
+        for(int i = 1; i < MAX+1; i++) {
+        	for(int j = 0; j<10; j++) {
+        		if(i == 1) {
+        			d[i][0] = 0;
+        		    d[i][j] = 1;
+        		    continue;
+        		}
+            	if(j == 0) {
+            		d[i][j] = d[i-1][1];
+            	}else if(j == 9) {
+            		d[i][j] = d[i-1][8];
+            	}else {
+            		d[i][j] = d[i-1][j-1]+d[i-1][j+1];
+            	}
+            	d[i][j]%=mod;
+            }	
+        }
+        long ans = 0;
+        for(int i = 0; i < 10; i++) {
+        	ans += d[n][i];
+        }
+        ans%=mod;
+        System.out.println(ans);
+    }
 }
